6a4dfb6f366dde8d1d3f38ef3a880a64737dcc8b
Rekku484 <137368315+Rekku484@users.noreply.github.com>
2025-03-12 01:46:07 +0800

    Completely rewritten HeaderView. Sidebar is now implemented

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,27 +1,29 @@
 <template>
     <div id="header">
-        <div class="nav">
+        <div class="nav-links">
             <RouterLink to="/"><img src='/src/assets/marsh.png' width="100"></RouterLink>
-        </div>
-        <div class="nav">
             <RouterLink to="/" style="text-decoration: none;"><p>Home</p></RouterLink>
-        </div>
-        <div class="nav">
             <RouterLink to="/artworks" style="text-decoration: none;"><p>Artworks</p></RouterLink> 
-        </div>
-        <div class="nav">
             <RouterLink to="/characters" style="text-decoration: none;"><p>Characters</p></RouterLink>
-        </div>
-        <div class="nav">
             <RouterLink to="/commission" style="text-decoration: none;"><p>Commission</p></RouterLink>
-        </div>
-        <div class="nav">
             <a href="https://github.com/Rekku484/Rekku484-portfolio-v2" style="text-decoration:none"><p>Github</p></a>
         </div>
         <div class="hamburger" @click="toggleSidebar"><Menu :size='24'></Menu></div>
     </div>
-    <div id="sidebar">
-        <div :class="{ 'isOpen':mobular }"></div>
+    
+    <div v-if="mobular" class="overlay" @click="toggleSidebar"></div>
+
+    <div id="sidebar" :class="{ 'isOpen': mobular }">
+        
+        <div class="sidebar-content">
+            <img src='/src/assets/marsh.png' width="140">
+            <RouterLink to="/" @click="toggleSidebar">Home</RouterLink>
+            <RouterLink to="/artworks" @click="toggleSidebar">Artworks</RouterLink> 
+            <RouterLink to="/characters" @click="toggleSidebar">Characters</RouterLink>
+            <RouterLink to="/commission" @click="toggleSidebar">Commission</RouterLink>
+            <a href="https://github.com/Rekku484/Rekku484-portfolio-v2" @click="toggleSidebar">Github</a>
+        </div>
+    
     </div>
 </template>
 
@@ -35,51 +37,83 @@ export default {
     },
     methods: {
         toggleSidebar() {
-            this.mobular = !this.mobular
+            this.mobular = !this.mobular;
         } 
     }
 }
-
 </script>
-
-<script setup lang="ts">
-import { Menu } from 'lucide-vue-next';
-
-</script>
-
-
 
 <style scoped>
 @media (max-width: 810px) {
-    
-}
+    .nav-links {
+        display: none !important
+    }
 
-@media (prefers-color-scheme: light) {
-    Menu {
-        color: black !important;
-        size: 24px;
+    .hamburger {
+        display: block;
+        cursor: pointer;
+        margin-left: auto;
+        padding: 1rem;
+        z-index: 4;
+    }
+
+    #sidebar {
+        position: fixed;
+        top: 0;
+        left: -100%;
+        width: 70%;
+        height: 100%;
+        background: #0A2328;
+        transition: left 0.3s ease-in-out;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 2rem;
+        z-index: 5;
+    }
+
+    #sidebar.isOpen {
+        left: 0;
+    }
+
+    .sidebar-content {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
+    .sidebar-content a {
+        text-decoration: none;
+        font-size: 1.5rem;
+    }
+
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0,0,0,0.25);
+        z-index: 3;
+        transition: 0.3s ease-in-out;
     }
 }
 
-Menu {
-    color: white;
+@media (min-width: 811px) {
+    #sidebar {
+    display: none !important;
+    }
+
+    .hamburger {
+        display: none;
+    }
+
+    .nav-links {
+        display: flex;
+        gap: 1rem;
+    }
 }
 
-#header {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-}
 
-.nav {
-    margin-top: 1rem;
-    margin-left: 1rem;
-}
-
-p {
-    font-size: 30px;
-    margin-left: 1rem;
-    margin-top: 1rem;
-}
 
 </style>
